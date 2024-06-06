fbad8783361263f894b5316b89a0187feb07bbda
Alethepata <alessia.patane@gmail.com>
2024-06-06 15:18:28 +0200

    add posts controller

---
+++ controllers/controllerPosts.js
@@ -0,0 +1,24 @@
+const index = (req, res) => {
+    
+}
+
+const show = (req, res) => {
+
+}
+const create = (req, res) => {
+
+}
+const update = (req, res) => {
+
+}
+const destroy = (req, res) => {
+
+}
+
+module.exports = {
+    index,
+    show,
+    create,
+    update,
+    destroy
+}
